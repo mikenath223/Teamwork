8762bd52e65639308208bd396fe3eb400b776d6c
mikenath223 <michgoldennathan17@gmail.com>
2020-08-28 10:51:07 +0100

    Update navbar with login details

--- src/components/Nav/Nav.styled.js
+++ src/components/Nav/Nav.styled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import { Button, Navbar, Nav } from 'react-bootstrap'
+import { Button, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { breakpoints } from 'styles/global';
 
 
@@ -7,10 +8,25 @@ export const Wrapper = styled(Navbar)`
   background-color: ${({ theme }) => theme.colors.white};
 `;
 
-export const LinkText = styled(Nav.Link)`
-  margin-right: 15px;
+export const LinkText = styled(Link)`
+  margin-right: 25px;
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   font-size: ${({ theme }) => theme.textSizes.large};
+
+  &:hover {
+  text-decoration: none;
+  }
+
+  &.nav-button {
+    border: none;
+    padding: 5px 8px;
+    color: ${({ theme }) => theme.colors.white};
+    background-color: ${({ theme }) => theme.colors.orange};
+  }
+
+  &.nav-button:hover {
+    opacity: 0.7;
+  }
 
   @media ${breakpoints.lg} {
     font-size: ${({ theme }) => theme.textSizes.regular};
@@ -40,4 +56,4 @@ export const NavButton = styled(Button)`
     opacity: 0.7;
     background-color: ${({ theme }) => theme.colors.orange}
   }
-`
+`;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { 
-  Wrapper,
-  NavButton,
-  Brand,
-  LinkText
-} from './Nav.styled'
+import Button from 'components/Button';
+import { Wrapper, Brand, LinkText } from './Nav.styled';
 
-const NavBar = () => (
-    <Wrapper expand="md px-5">
-      <Brand href="#home">teamwork</Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mx-auto">
-          <LinkText href="#">Why teamwork?</LinkText>
-          <LinkText href="#">Solutions?</LinkText>
-          <LinkText href="#">Pricing?</LinkText>
-          <LinkText href="#">Resources</LinkText>
-        </Nav>
-        <Nav className="mr-3">
-          <LinkText href="#">Login</LinkText>
-          <NavButton className="border border-0 btn-warning">Try Now</NavButton>
-        </Nav>
-      </Navbar.Collapse>
-    </Wrapper>
+const NavBar = ({ user: { photoURL, displayName, email }, signOut }) => (
+  <Wrapper expand="md px-5">
+    <Brand href="#home">teamwork</Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mx-auto">
+        <LinkText to="/meetings">Meetings</LinkText>
+        <LinkText to="/chats">Chats</LinkText>
+        <LinkText to="/calendar">Calendar</LinkText>
+      </Nav>
+      <Nav className="mr-3">
+        {email ? (
+          <>
+            <img src={photoURL || ''} alt={displayName || ''} />
+            <p>{displayName || ''}</p>
+            <p>{email}</p>
+            <Button className="btn btn-warning border-0" onClick={signOut}>
+              Logout
+            </Button>
+          </>
+        ) : (
+            <>
+              <LinkText to="/auth/signin">Sign In</LinkText>
+              <LinkText className="nav-button" to="/auth/signup">Try Now</LinkText>
+            </>
+          )}
+      </Nav>
+    </Navbar.Collapse>
+  </Wrapper>
 );
 
 export default NavBar;
