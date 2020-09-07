2665d90c42c0c8ddb7264fb902b20322f3c82158
mikenath223 <michgoldennathan17@gmail.com>
2020-09-07 10:15:07 +0100

    Create closure function to sort chats

--- src/pages/Chats/Chats.styled.js
+++ src/pages/Chats/Chats.styled.js
@@ -1,4 +1,13 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+
+  & > *:first-child {
+    margin-left: 10%;
+  }
+  & > *:nth-child(2) {
+    margin-right: 10%;
+  }
 `;
--- src/pages/Chats/index.js
+++ src/pages/Chats/index.js
@@ -1,33 +1,97 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  selectUsersData,
-  saveUsers
-} from 'store/slices/usersListSlice'
+import { selectUsersData, setUsers } from 'store/slices/usersListSlice';
+import { selectChatData, setChats } from 'store/slices/chatSlice'
+import { selectCurrentUser } from 'store/slices/currentUserSlice';
+import { getUsers, getChats, sendFirestoreChat } from 'utils/request';
 import { Wrapper } from './Chats.styled';
-import { getUsers } from 'utils/request';
 import UsersList from 'components/UsersList';
+import ChatFrame from 'components/ChatFrame';
 
+const Chats = ({ user }) => {
+  const [message, setMessage] = useState('');
+  const [recieverId, setRecieverId] = useState('');
+  const [sub, setSub] = useState({});
 
-const Chats = () => {
   const dispatch = useDispatch();
   const usersData = useSelector(selectUsersData);
+  const chatData = useSelector(selectChatData);
+  const currentUser = useSelector(selectCurrentUser);
 
   const retrieveUsers = useCallback(async () => {
-    const users = await getUsers();
-    dispatch(saveUsers(users));
-  }, [dispatch]);
+    let users = await getUsers();
+    users = users.filter(item => item.id !== user);
+    dispatch(setUsers(users));
+  }, [dispatch, user]);
 
   useEffect(() => {
-    if (usersData.length === 0) {
+    if (usersData.length === 0 && user) {
       retrieveUsers();
     }
-  }, [retrieveUsers, usersData.length])
 
+    return () => {
+      // if (sub)
+      sub.senderSub();
+      sub.recieverSub();
+    }
+  }, [retrieveUsers, usersData.length, user]);
+
+  const handleGetUserChats = async (recieverId) => {
+    setRecieverId(recieverId);
+    if (sub.recieverSub || sub.senderSub) {
+      sub.senderSub();
+      sub.recieverSub();
+    }
+    const runDispatch = () => {
+      let chats = [], count = 0
+
+      return {
+        loadChat: (input) => {
+          if (count >= 2) {
+            count = 0;
+            chats = [];
+          }
+          if (chats.length) {
+            chats.push(...input);
+            chats = chats.sort((a, b) => a.date - b.date)
+            dispatch(setChats(chats));
+          } else {
+            chats.push(...input);
+          }
+          count += 1;
+        }
+      }
+    };
+    const runIt = runDispatch();
+    let { sub: recieverSub } = await getChats(recieverId, user, runIt);
+    let { sub: senderSub } = await getChats(user, recieverId, runIt);
+    setSub({ senderSub, recieverSub });
+  }
+
+  const handleSendUserChat = () => {
+    if (message && recieverId) {
+      console.log('SendChat', message, user, recieverId);
+      sendFirestoreChat(message, user, recieverId);
+      setMessage('');
+    }
+  };
+
+  const handleChange = (event) => {
+    const { target: { value } } = event;
+    setMessage(value);
+  }
 
   return (
     <Wrapper>
-      <UsersList users={usersData} />
+      <UsersList
+        users={usersData}
+        getUserChats={handleGetUserChats} />
+      <ChatFrame
+        chats={chatData}
+        handleSendChat={handleSendUserChat}
+        inputMessage={message}
+        currentUserId={user}
+        handleChange={handleChange} />
     </Wrapper>
   )
 }
